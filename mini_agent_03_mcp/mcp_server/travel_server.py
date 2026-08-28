761bbae7e23ba2149080b7ebd5c27636d062fcd7
youngmin <sym06040@gmail.com>
2026-08-28 14:27:16 +0900

    0828

--- mini_agent_03_mcp/mcp_server/travel_server.py
+++ mini_agent_03_mcp/mcp_server/travel_server.py
@@ -6,7 +6,7 @@ from typing import Literal
 from mcp.server.fastmcp import FastMCP
 
 
-MCP_HOST = os.getenv("MCP_HOST", "127.0.0.1")
+MCP_HOST = os.getenv("MCP_HOST", "192.100.200.198")
 MCP_PORT = int(os.getenv("MCP_PORT", "8010"))
 
 mcp = FastMCP(
